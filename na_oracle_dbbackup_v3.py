0441de566edaebaf82e5f64b3a47876840dd6d0c
mikeburt1213 <mike.burt@netapp.com>
2025-02-01 12:02:15 -0600

    update

--- na_oracle_dbbackup_v3.py
+++ na_oracle_dbbackup_v3.py
@@ -304,7 +304,7 @@ def clone_lun(args) -> None:
                         print("Clone LUN S/N Refresh Completed")
                         print("Clone LUN S/N: " + clone_serial_number)
                         print("======================================================================")
-                    clone_lun.enable = 'false'
+                    clone_lun.enabled = 'false'
                     if clone_lun.patch():
                         print("======================================================================")
                         print("Clone LUN Offline Complete")
